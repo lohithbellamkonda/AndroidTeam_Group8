f2ad3ce8f4532b425b839d83e5f5b0c34ee59fe5
Corey Unruh <113541726+CUnruh641@users.noreply.github.com>
2023-10-06 22:05:03 -0500

    Added more description comments

--- AndroidProject/app/src/main/java/com/example/game/PauseEndScreen.java
+++ AndroidProject/app/src/main/java/com/example/game/PauseEndScreen.java
@@ -14,4 +14,6 @@ public class PauseEndScreen extends AppCompatActivity {
 
     //Pause Screen to go back to main menu will be here
     //End Game Screen when player beats the game will be here
+    //Will show how long user took to beat the game(will only run when the user is playing a level)
+    //Previous line may change if it is possible to do a seamless transition between levels
 }
