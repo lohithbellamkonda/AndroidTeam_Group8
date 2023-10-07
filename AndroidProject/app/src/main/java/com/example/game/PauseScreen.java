b04ea301fdfad49770044105b36d4bcd5d891751
Corey Unruh <113541726+CUnruh641@users.noreply.github.com>
2023-10-06 22:54:05 -0500

    Added pause and end screens
    Added end screen class and layout. Added two TextViews and two Buttons to  pause screen layout

---
+++ AndroidProject/app/src/main/java/com/example/game/PauseScreen.java
@@ -0,0 +1,16 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PauseScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pause_screen);
+    }
+
+    //Pause Screen to go back to main menu or resume the game will be here
+}
